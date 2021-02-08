9bcd051bd34a01260854f94ac82d48622fb6572a
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-02-08 20:34:59 +0100

    Change folder structure and add eslint/prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+      <p>Hello again</p>
+    </div>
+  );
+};
+
+export default App;
