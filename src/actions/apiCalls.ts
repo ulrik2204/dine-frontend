65071fdc463ee14f89f83b57e4645bd97184e77b
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-02-19 12:02:40 +0100

    Change the POST hook to return av Post function as well

--- src/actions/apiCalls.ts
+++ src/actions/apiCalls.ts
@@ -7,24 +7,23 @@ import { Dinner } from '../util/types';
  * @return a variable the fetched data will be placed in, and a function to get the data
  */
 /* eslint-disable no-unused-vars */
-export const useFetchFromAPI = (): [Dinner[] | undefined, (urlPath: string) => void] => {
-  const [state, setState] = useState<Dinner[]>();
+export const useGetFromAPI = (): [Dinner[] | undefined, (urlPath: string) => void] => {
+  const [data, setData] = useState<Dinner[]>();
 
   // The actual GET request for a URL
-  const getDinner = useCallback(
+  const getDinnerData = useCallback(
     (urlPath: string) => {
       axios
         .get<Dinner[]>(urlPath)
         // After the response is recieved, take that data and put it in a state
-        .then((res) => setState(res.data))
+        .then((res) => setData(res.data))
         .catch((err) => console.log(err));
     },
-    [setState],
+    [setData],
   );
   // Return that state
-  return [state, getDinner];
+  return [data, getDinnerData];
 };
-/* eslint-enable no-unused-vars */
 
 /**
  * A function for posting a Dinner to the API
@@ -32,21 +31,24 @@ export const useFetchFromAPI = (): [Dinner[] | undefined, (urlPath: string) => v
  * @param dinner The dinner object to post
  * @return Status code as a number, indicating if the request was successful
  */
-export const postDinnerToAPI = (urlPath: string, dinner: Dinner): number => {
-  let statusCode = 0;
+export const usePostToAPI = (): [number | undefined, (urlPath: string, dinner: Dinner) => void] => {
+  const [status, setStatus] = useState<number>();
   const headers = {
     'Content-Type': 'application/json',
   };
   // The actual post request
-  console.log(JSON.stringify(dinner));
-  axios
-    .post(urlPath, JSON.stringify(dinner), {
-      headers: headers,
-    })
-    // After a response is recieved, retrieve its status code
-    .then((res) => {
-      statusCode = res.status;
-    })
-    .catch((err) => console.log(err));
-  return statusCode;
+  const postDinner = useCallback(
+    (urlPath: string, dinner: Dinner): void => {
+      axios
+        .post(urlPath, JSON.stringify(dinner), {
+          headers: headers,
+        })
+        // After a response is recieved, retrieve its status code
+        .then((res) => setStatus(res.status))
+        .catch((err) => console.log(err));
+    },
+    [setStatus],
+  );
+  return [status, postDinner];
 };
+/* eslint-enable no-unused-vars */
