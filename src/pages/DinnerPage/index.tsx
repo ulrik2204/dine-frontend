8854526acb459b6ae4bcaf8b6ef4ec205157d493
Sigrid <sigridli2003@gmail.com>
2021-02-20 16:23:59 +0100

    Create a style for dinner event page

--- src/pages/DinnerPage/index.tsx
+++ src/pages/DinnerPage/index.tsx
@@ -6,26 +6,24 @@ import '../../fonts/Roboto-Thin.ttf';
  * Component for dinner page.
  */
 const DinnerPage: React.FunctionComponent = () => {
-  const dinner = { course: 'pasta med reker', host: 'Simen', dateTime: 'now', kitchen: 'italiensk' };
+  const dinner = { course: 'Taco', host: 'Simen', dateTime: 'now', kitchen: 'Meksikansk' };
 
   return (
     <div className="dinnerPageContainer">
-      <h1 className="courseHeadline">{dinner.course}</h1>
+      <h1 className="dinnerHeadline">{dinner.course}</h1>
       <img
         src="https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
         alt="Matbilde"
       />
       <h1>Vert</h1>
       <h3>{dinner.host}</h3>
-      <br />
 
-      <div className="dinnerTimeDate">
-        <h1>Tidspunkt</h1>
-        <h3>{dinner.dateTime}</h3>
+      <h1>Kjøkken</h1>
+      <h3>{dinner.kitchen}</h3>
 
-        <h1>Kjøkken</h1>
-        <h3>{dinner.kitchen}</h3>
-      </div>
+      <h1>Tidspunkt</h1>
+      <h3>{dinner.dateTime}</h3>
+
       <button id="signUp" type="submit">
         Meld på
       </button>
