ca397d01813a0ecb7eea759b0983770d3bf1ff13
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-02-24 00:56:38 +0100

    Logo now redirects to Overview on click

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Menu.css';
 import { ReactComponent as ReactLogo } from '../../assets/dine_logo.svg';
 import { AppBar, createStyles, makeStyles, Theme } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     appbar: {
@@ -17,11 +18,14 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 const Menu: React.FunctionComponent = () => {
   const classes = useStyles();
+  const history = useHistory();
   return (
     <div>
       <AppBar position="fixed" className={classes.appbar}>
-        <ReactLogo id="headerLogo" />
-        <h1 id="header">dine</h1>
+        <ReactLogo id="headerLogo" onClick={() => history.push('/')} />
+        <h1 id="header" onClick={() => history.push('/')}>
+          dine
+        </h1>
       </AppBar>
     </div>
   );
