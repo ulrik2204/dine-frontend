6b5329bcfbda0ca82f9012944c8398dabbf11150
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-02-23 10:47:33 +0100

    Create basic overview page

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useGetFromAPI } from '../../actions/apiCalls';
+import './Overview.css';
 const Overview: React.FunctionComponent = () => {
-  return <div>Here comes the overview page</div>;
+  const [data, getData] = useGetFromAPI();
+
+  useEffect(() => {
+    getData('/api/');
+    console.log('Render');
+  }, []);
+  return (
+    <div id="overviewDiv">
+      <h1 className="title">Oversikt</h1>
+      {(() => {
+        const content: JSX.Element[] = [];
+        if (data != undefined) {
+          data.forEach((dinner) => {
+            content.push(
+              <p key={dinner.id} className="dinnerElement">
+                {JSON.stringify(dinner)}
+              </p>,
+            );
+          });
+        }
+        return content;
+      })()}
+    </div>
+  );
 };
 export default Overview;
