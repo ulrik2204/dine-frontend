ddca152a95bd7b5cb3690884369f666544135957
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-03-04 19:34:40 +0100

    Centralize css for DinnerPage

--- src/pages/CreateDinnerPage/index.tsx
+++ src/pages/CreateDinnerPage/index.tsx
@@ -6,7 +6,9 @@ import { usePostToAPI } from '../../actions/apiCalls';
 import { Dinner } from '../../util/types';
 import { useHistory } from 'react-router-dom';
 import useDidMountEffect from '../../actions/useDidMountEffect';
-import useCreateDinnerPageStyles from './stylesCreateDinnerPage';
+// import useCreateDinnerPageStyles from './stylesCreateDinnerPage';
+import styles from './styles.module.css';
+import { StylesProvider } from '@material-ui/core/styles';
 
 /**
  * The component page for creating a dinner element
@@ -23,7 +25,6 @@ const CreateDinnerPage: React.FunctionComponent = () => {
   const [usedStatus, setUsedStatus] = useState<number>();
   // history and css
   const history = useHistory();
-  const classes = useCreateDinnerPageStyles();
 
   // The function for taking in the form input and sening it as a post request to the backend
   const sendForm = useCallback((dish: string, cuisine: string, date: string, location: string, owner: string) => {
@@ -64,68 +65,70 @@ const CreateDinnerPage: React.FunctionComponent = () => {
   }, [usedStatus, setUsedStatus]);
 
   return (
-    <div className={classes.createDinnerContainer}>
-      <h1 className={'title'}>Opprett Middag</h1>
-      <h2 className={classes.createDinnerH2 + ' ' + classes.createDinnerH12}>Rett</h2>
-      <TextField
-        className={classes.inputField}
-        value={dish}
-        onChange={(event) => setDish(event.target.value)}
-      ></TextField>
-      <br></br>
-      <h2 className={classes.createDinnerH2 + ' ' + classes.createDinnerH12}>Kjøkken</h2>
-      <NativeSelect className={classes.inputField} onChange={(e) => setCuisine(e.target.value)}>
-        <option value={'Andre'}>Andre</option>
-        <option value={'Fransk'}>Fransk</option>
-        <option value={'Indisk'}>Indisk</option>
-        <option value={'Italiensk'}>Italiensk</option>
-        <option value={'Japansk'}>Japansk</option>
-        <option value={'Kinesisk'}>Kinesisk</option>
-        <option value={'Meksikansk'}>Meksikansk</option>
-        <option value={'Norsk'}>Norsk</option>
-      </NativeSelect>
-      <br></br>
-      <h2 className={classes.createDinnerH2 + ' ' + classes.createDinnerH12}>Tidspunkt</h2>
-      <form noValidate>
+    <StylesProvider injectFirst>
+      <div className={styles.createDinnerContainer}>
+        <h1 className={'title'}>Opprett Middag</h1>
+        <h2 className={styles.createDinnerH2}>Rett</h2>
         <TextField
-          onChange={(event) => setDateTime(event.target.value)}
-          id="datetime-local"
-          label="Next appointment"
-          type="datetime-local"
-          value={dateTime}
-          className={classes.inputField}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </form>
-      <br></br>
-      <h2 className={classes.createDinnerH2 + ' ' + classes.createDinnerH12}>Sted</h2>
-      <TextField
-        className={classes.inputField}
-        value={location}
-        onChange={(event) => setLocation(event.target.value)}
-      ></TextField>
-      <br></br>
-      <h2 className={classes.createDinnerH2 + ' ' + classes.createDinnerH12}>Vert</h2>
-      <TextField
-        className={classes.inputField}
-        value={owner}
-        onChange={(event) => setOwner(event.target.value)}
-      ></TextField>
-      <br />
-      <br></br>
-      <div className={classes.buttonDiv}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => sendForm(dish, cuisine, dateTime, location, owner)}
-          className={classes.buttonField}
-        >
-          Opprett
-        </Button>
+          className={styles.inputField}
+          value={dish}
+          onChange={(event) => setDish(event.target.value)}
+        ></TextField>
+        <br></br>
+        <h2 className={styles.createDinnerH2}>Kjøkken</h2>
+        <NativeSelect className={styles.inputField} onChange={(e) => setCuisine(e.target.value)}>
+          <option value={'Andre'}>Andre</option>
+          <option value={'Fransk'}>Fransk</option>
+          <option value={'Indisk'}>Indisk</option>
+          <option value={'Italiensk'}>Italiensk</option>
+          <option value={'Japansk'}>Japansk</option>
+          <option value={'Kinesisk'}>Kinesisk</option>
+          <option value={'Meksikansk'}>Meksikansk</option>
+          <option value={'Norsk'}>Norsk</option>
+        </NativeSelect>
+        <br></br>
+        <h2 className={styles.createDinnerH2}>Tidspunkt</h2>
+        <form noValidate>
+          <TextField
+            onChange={(event) => setDateTime(event.target.value)}
+            id="datetime-local"
+            label="Next appointment"
+            type="datetime-local"
+            value={dateTime}
+            className={styles.inputField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </form>
+        <br></br>
+        <h2 className={styles.createDinnerH2}>Sted</h2>
+        <TextField
+          className={styles.inputField}
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+        ></TextField>
+        <br></br>
+        <h2 className={styles.createDinnerH2}>Vert</h2>
+        <TextField
+          className={styles.inputField}
+          value={owner}
+          onChange={(event) => setOwner(event.target.value)}
+        ></TextField>
+        <br />
+        <br></br>
+        <div className={styles.buttonDiv}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => sendForm(dish, cuisine, dateTime, location, owner)}
+            className={styles.buttonField}
+          >
+            Opprett
+          </Button>
+        </div>
       </div>
-    </div>
+    </StylesProvider>
   );
 };
 export default CreateDinnerPage;
--- src/pages/DinnerPage/index.tsx
+++ src/pages/DinnerPage/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import './DinnerPage.css';
 import '../../fonts/Roboto-Thin.ttf';
 import { Dinner } from '../../util/types';
 import { useGetFromAPI } from '../../actions/apiCalls';
+import useDinnerPageStyles from './stylesDinnerPage';
 
 // All you need to see a dinner page is the dinnerID
 type DinnerPageProps = {
@@ -14,31 +14,40 @@ type DinnerPageProps = {
  */
 const DinnerPage: React.FunctionComponent<DinnerPageProps> = (props: DinnerPageProps) => {
   const [dinner, getDinner] = useGetFromAPI();
+  const classes = useDinnerPageStyles();
   useEffect(() => {
     getDinner(`/api/${props.dinnerID}`);
   }, []);
   return (
-    <div className="dinnerPageContainer">
-      <h1 id="dinnerPageHeadline">{dinner == undefined ? '' : (dinner as Dinner).dish}</h1>
+    <div className={classes.dinnerPageContainer}>
+      <h1 className="title">{dinner == undefined ? '' : (dinner as Dinner).dish}</h1>
       <img
-        id="dinnerPageImage"
+        className={classes.dinnerPageImage + ' ' + classes.halfWidthLeft}
         src="https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
         alt="Matbilde"
       />
-      <h1 className="dinnerPageH1">Vert</h1>
-      <h3 className="dinnerPageH3">{dinner == undefined ? '' : (dinner as Dinner).owner}</h3>
+      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Vert</h1>
+      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
+        {dinner == undefined ? '' : (dinner as Dinner).owner}
+      </h3>
 
-      <h1 className="dinnerPageH1">Kjøkken</h1>
-      <h3 className="dinnerPageH3">{dinner == undefined ? '' : (dinner as Dinner).cuisine}</h3>
+      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Kjøkken</h1>
+      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
+        {dinner == undefined ? '' : (dinner as Dinner).cuisine}
+      </h3>
 
-      <h1 className="dinnerPageH1">Tidspunkt</h1>
-      <h3 className="dinnerPageH3">{dinner == undefined ? '' : new Date((dinner as Dinner).date).toLocaleString()}</h3>
+      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Tidspunkt</h1>
+      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
+        {dinner == undefined ? '' : new Date((dinner as Dinner).date).toLocaleString()}
+      </h3>
 
-      <h1 className="dinnerPageH1">Sted</h1>
-      <h3 className="dinnerPageH3">{dinner == undefined ? '' : (dinner as Dinner).location}</h3>
+      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Sted</h1>
+      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
+        {dinner == undefined ? '' : (dinner as Dinner).location}
+      </h3>
 
       {/* 
-      <button id="signUp" type="submit">
+      <button className={classes.signUp}>
         Meld på
       </button> */}
     </div>
