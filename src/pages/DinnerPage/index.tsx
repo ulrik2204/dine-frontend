6e6d1c1cfb4f2839bbb4b7eefe0d26f792f073a9
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-03-07 23:48:10 +0100

    Change DinnePage styling to a module css file

--- src/pages/DinnerPage/index.tsx
+++ src/pages/DinnerPage/index.tsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import '../../fonts/Roboto-Thin.ttf';
 import { Dinner } from '../../util/types';
 import { useGetFromAPI } from '../../actions/apiCalls';
-import useDinnerPageStyles from './stylesDinnerPage';
+// import useDinnerPageStyles from './stylesDinnerPage';
+import styles from './styles.module.css';
 
 // All you need to see a dinner page is the dinnerID
 type DinnerPageProps = {
@@ -14,37 +15,31 @@ type DinnerPageProps = {
  */
 const DinnerPage: React.FunctionComponent<DinnerPageProps> = (props: DinnerPageProps) => {
   const [dinner, getDinner] = useGetFromAPI();
-  const classes = useDinnerPageStyles();
+  // const styles = useDinnerPageStyles();
   useEffect(() => {
     getDinner(`/api/${props.dinnerID}`);
   }, []);
   return (
-    <div className={classes.dinnerPageContainer}>
+    <div className={styles.dinnerPageContainer}>
       <h1 className="title">{dinner == undefined ? '' : (dinner as Dinner).dish}</h1>
       <img
-        className={classes.dinnerPageImage + ' ' + classes.halfWidthLeft}
+        className={styles.dinnerPageImage}
         src="https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
         alt="Matbilde"
       />
-      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Vert</h1>
-      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
-        {dinner == undefined ? '' : (dinner as Dinner).owner}
-      </h3>
+      <h1 className={styles.dinnerPageH1}>Vert</h1>
+      <h3 className={styles.dinnerPageH3}>{dinner == undefined ? '' : (dinner as Dinner).owner}</h3>
 
-      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Kjøkken</h1>
-      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
-        {dinner == undefined ? '' : (dinner as Dinner).cuisine}
-      </h3>
+      <h1 className={styles.dinnerPageH1}>Kjøkken</h1>
+      <h3 className={styles.dinnerPageH3}>{dinner == undefined ? '' : (dinner as Dinner).cuisine}</h3>
 
-      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Tidspunkt</h1>
-      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
+      <h1 className={styles.dinnerPageH1}>Tidspunkt</h1>
+      <h3 className={styles.dinnerPageH3}>
         {dinner == undefined ? '' : new Date((dinner as Dinner).date).toLocaleString()}
       </h3>
 
-      <h1 className={classes.dinnerPageH1 + ' ' + classes.halfWidthLeft}>Sted</h1>
-      <h3 className={classes.dinnerPageH3 + ' ' + classes.halfWidthLeft}>
-        {dinner == undefined ? '' : (dinner as Dinner).location}
-      </h3>
+      <h1 className={styles.dinnerPageH1}>Sted</h1>
+      <h3 className={styles.dinnerPageH3}>{dinner == undefined ? '' : (dinner as Dinner).location}</h3>
 
       {/* 
       <button className={classes.signUp}>
--- src/pages/DinnerPage/stylesDinnerPage.ts
+++ src/pages/DinnerPage/stylesDinnerPage.ts
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core';
 
+
 const useDinnerPageStyles = makeStyles({
   dinnerPageContainer: {
     display: 'flex',
