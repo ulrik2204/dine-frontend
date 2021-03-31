1b3c86e4cbf195b7be8bfee620efdbac0820731c
Haakon Selnes <hselnes@hotmail.com>
2021-03-31 18:18:38 +0200

    work on tests

--- src/testing/LogIn.test.tsx
+++ src/testing/LogIn.test.tsx
@@ -5,25 +5,36 @@ import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import LogInPage from '../pages/LogIn/index';
 import { Button } from '@material-ui/core';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 
 configure({ adapter: new Adapter() });
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
 
 describe('Testing LogIn Page', () => {
   let mount: any;
   let wrapper: any;
   let button: any;
   let registerLink: any;
+  const history = createMemoryHistory();
 
   //The input fields
   let usernameInput: any;
   let passwordInput: any;
+
   beforeEach(() => {
     mount = createMount();
     wrapper = mount(
-      <div>
+      <Router history={history}>
         <ToastContainer />
         <LogInPage />
-      </div>,
+      </Router>,
     );
     usernameInput = screen.getByPlaceholderText('brukernavn');
     passwordInput = screen.getByPlaceholderText('passord');
@@ -41,6 +52,10 @@ describe('Testing LogIn Page', () => {
     fireEvent.change(passwordInput, { target: { value: '' } });
     button.simulate('click');
     await screen.findByText('Du må skrive inn begge feltene');
+  });
+  test('Testing clicking register redirects to register page', async () => {
+    fireEvent.click(registerLink);
+    expect(history.location.pathname).toBe('/regin');
   });
 
   // Clean up after the tests
