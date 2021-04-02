f9f076ea1aac6b0524e71df1bf5b30d137b8d56d
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-04-02 17:25:59 +0200

    Add retrieve users func and update overview

--- src/actions/retrieve.ts
+++ src/actions/retrieve.ts
@@ -1,5 +1,5 @@
-import axios from '../myaxios';
 import { useEffect, useState } from 'react';
+import axios from '../myaxios';
 
 /**
  * A hook to retrieve a list of the names of allergies from a list of allergy ids.
@@ -12,9 +12,22 @@ export const retrieveAllergies = (allergyIDs: number[]): string[] => {
   useEffect(() => {
     const promises: Promise<string>[] = [];
     allergyIDs?.forEach((id) => {
-      promises.push(axios.get(`/api/allergies/${id}`).then((res) => res.data.allergy));
+      promises.push(axios.get(`/api/allergies/${id}/`).then((res) => res.data.allergy));
     });
     Promise.all(promises).then((res) => setAllergies(res));
   }, [allergyIDs]);
   return allergies;
+};
+
+export const retrieveUsers = (userIDs: number[]): string[] => {
+  const [users, setUsers] = useState<string[]>([]);
+
+  useEffect(() => {
+    const promises: Promise<string>[] = [];
+    userIDs?.forEach((id) => {
+      promises.push(axios.get(`/api/users/${id}/`).then((res) => `${res.data.first_name} ${res.data.last_name}`));
+    });
+    Promise.all(promises).then((res) => setUsers(res));
+  });
+  return users;
 };
